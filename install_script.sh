213aedd01da5a80df31a94b045fd952864d3dd89
cyber <cyber610@pm.me>
2025-06-03 16:04:07 -0700

    batman

---
+++ install_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install Neovim
+
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.appimage
+
+chmod u+x nvim-linux-x86_64.appimage
+
+sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
+
+# end of Neovim install
+ 
